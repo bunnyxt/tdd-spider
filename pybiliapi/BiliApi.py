d46e635c0503749b3b1a20527fa1537662e0c466
bunnyxt <bunnyxt@outlook.com>
2019-09-22 14:32:47 +0800

    Add pybiliapi module.

---
+++ pybiliapi/BiliApi.py
@@ -0,0 +1,47 @@
+import urllib3
+import json
+
+__all__ = ['BiliApi']
+
+# disable InsecureRequestWarning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class BiliApi:
+    """core class for api call"""
+
+    def __init__(self):
+        self.http = urllib3.PoolManager()
+
+    def url_request(self, method, url):
+        if method in ['GET']:
+            response = self.http.request(method, url)
+            if response.status == 200:
+                try:
+                    html = response.data.decode()
+                    obj = json.loads(html)
+                    return obj
+                except Exception as e:
+                    print(e)
+                    return None
+            else:
+                print('Error! Fail to get response with status code %d.' % response.status)
+                return None
+        else:
+            print('Error! Method %s not support.' % method)
+            return None
+
+    def get_video_view(self, aid):
+        return self.url_request('GET', 'https://api.bilibili.com/x/web-interface/view?aid={0}'.format(aid))
+
+    def get_video_tags(self, aid):
+        return self.url_request('GET', 'https://api.bilibili.com/x/tag/archive/tags?aid={0}'.format(aid))
+
+    def get_video_stat(self, aid):
+        return self.url_request('GET', 'https://api.bilibili.com/x/web-interface/archive/stat?aid={0}'.format(aid))
+
+    def get_member(self, mid):
+        return self.url_request('GET', 'https://api.bilibili.com/x/space/acc/info?mid={0}'.format(mid))
+
+    def get_member_relation(self, mid):
+        return self.url_request('GET', 'https://api.bilibili.com/x/relation/stat?vmid={0}'.format(mid))
