14806b7201f02d89be592cbe507eeb6c0d82f9d7
bunnyxt <bunnyxt@outlook.com>
2019-10-23 17:42:12 +0800

    Add logger via logging.

---
+++ logger/logger.py
@@ -0,0 +1,112 @@
+import logging
+import logging.config
+import os
+
+__all__ = ['BASE_DIR', 'LOG_DIR', 'logger_01', 'logger_db']
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+LOG_DIR = os.path.join(BASE_DIR, 'log')
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+
+class InfoPlusFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelno >= logging.INFO:
+            return super().filter(record)
+        else:
+            return 0
+
+
+class WarningPlusFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelno >= logging.WARNING:
+            return super().filter(record)
+        else:
+            return 0
+
+
+LOG_CONFIG_DICT = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'class': 'logging.Formatter',
+            'format': '[%(asctime)s][%(name)s][%(levelname)s]: %(message)s'
+        }
+    },
+    'filters': {
+        'info_plus_filter': {
+            '()': InfoPlusFilter
+        },
+        'warning_plus_filter': {
+            '()': WarningPlusFilter
+        }
+    },
+    'handlers': {
+        'console_info': {
+            'level': 'INFO',
+            'formatter': 'simple',
+            'class': 'logging.StreamHandler'
+        },
+        'file_info_01': {
+            'level': 'INFO',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, '01_info.log'),
+            'when': "d",
+            'interval': 1,
+            'encoding': 'utf8',
+            'backupCount': 30,
+            'filters': ['info_plus_filter']
+        },
+        'file_warning_01': {
+            'level': 'WARNING',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, '01_warning.log'),
+            'when': "d",
+            'interval': 1,
+            'encoding': 'utf8',
+            'backupCount': 30,
+            'filters': ['warning_plus_filter']
+        },
+        'file_info_db': {
+            'level': 'INFO',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, 'db_info.log'),
+            'when': "d",
+            'interval': 1,
+            'encoding': 'utf8',
+            'backupCount': 30,
+            'filters': ['info_plus_filter']
+        },
+        'file_warning_db': {
+            'level': 'WARNING',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, 'db_warning.log'),
+            'when': "d",
+            'interval': 1,
+            'encoding': 'utf8',
+            'backupCount': 30,
+            'filters': ['warning_plus_filter']
+        }
+    },
+    'loggers': {
+        'logger_01': {
+            'handlers': ['console_info', 'file_info_01', 'file_warning_01'],
+            'level': 'INFO'
+        },
+        'logger_db': {
+            'handlers': ['console_info', 'file_info_db', 'file_warning_db'],
+            'level': 'INFO'
+        }
+    }
+}
+
+logging.config.dictConfig(LOG_CONFIG_DICT)
+
+logger_01 = logging.getLogger('logger_01')
+logger_db = logging.getLogger('logger_db')
