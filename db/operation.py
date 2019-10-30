6ae2153a4bbb56dbc8ef135d64fc1d8c36addd8a
bunnyxt <bunnyxt@outlook.com>
2019-10-30 11:11:03 +0800

    Add operations.

--- db/operation.py
+++ db/operation.py
@@ -1,3 +1,6 @@
+from logger import logger_db
+from .models import TddVideo
+
 __all__ = ['DBOperation']
 
 
@@ -9,5 +12,23 @@ class DBOperation:
             result = session.query(table).all()
             return result
         except Exception as e:
-            print(e)
+            logger_db.error('Exception: %s, params: %s' % (e, {'table': table}), exc_info=True)
             return None
+
+    @classmethod
+    def query_tdd_video_via_aid(cls, aid, session):
+        try:
+            result = session.query(TddVideo).filter(TddVideo.aid == aid).first()
+            return result
+        except Exception as e:
+            logger_db.error('Exception: %s, params: %s' % (e, {'aid': aid}), exc_info=True)
+            return None
+
+    @classmethod
+    def add(cls, obj, session):
+        try:
+            session.add(obj)
+            session.commit()
+        except Exception as e:
+            logger_db.error('Exception: %s, params: %s' % (e, {'obj': obj}), exc_info=True)
+            session.rollback()
