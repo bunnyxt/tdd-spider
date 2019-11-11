160fa6516af0c2e1ac2e77ee9fab094938de46fa
bunnyxt <bunnyxt@outlook.com>
2019-11-11 17:28:43 +0800

    Add test_video_view and test_video_stat; fix test_archive_rank_by_partion return bug.

--- common/validation.py
+++ common/validation.py
@@ -1,12 +1,12 @@
 import time
 
-__all__ = ['get_valid', 'test_archive_rank_by_partion']
+__all__ = ['get_valid', 'test_video_view', 'test_video_stat', 'test_archive_rank_by_partion']
 
 
 def get_valid(get_obj_func, get_obj_func_args, test_obj_func, repeat_count=5, colddown=1):
     result = None
     request_count = 1
-    while repeat_count <= repeat_count:
+    while request_count <= repeat_count:
         try:
             obj = get_obj_func(*get_obj_func_args)
             err = test_obj_func(obj)
@@ -25,26 +25,58 @@ def get_valid(get_obj_func, get_obj_func_args, test_obj_func, repeat_count=5, co
     return result
 
 
-def test_archive_rank_by_partion(obj):
+def test_video_view(obj):
     try:
         # obj should be a dictionary
         if type(obj) is not dict:
-            return False, 'Obj should be a dictionary.'
+            return 'Obj should be a dictionary.'
 
         # contains key code, message, ttl, data
         for key in ['code', 'message', 'ttl', 'data']:
             if key not in obj.keys():
-                return False, 'Obj should contain key %s.' % key
+                return 'Obj should contain key %s.' % key
+
+        return None
+    except Exception as e:
+        return str(e)
+
+
+def test_video_stat(obj):
+    try:
+        # obj should be a dictionary
+        if type(obj) is not dict:
+            return 'Obj should be a dictionary.'
+
+        # contains key code, message, ttl, data
+        for key in ['code', 'message', 'ttl', 'data']:
+            if key not in obj.keys():
+                return 'Obj should contain key %s.' % key
+
+        return None
+    except Exception as e:
+        return str(e)
+
+
+def test_archive_rank_by_partion(obj):
+    try:
+        # obj should be a dictionary
+        if type(obj) is not dict:
+            return 'Obj should be a dictionary.'
+
+        # contains key code, message, ttl, data
+        for key in ['code', 'message', 'ttl', 'data']:
+            if key not in obj.keys():
+                return 'Obj should contain key %s.' % key
 
         # data contains archives and page
         for key in ['archives', 'page']:
             if key not in obj['data'].keys():
-                return False, 'Obj[\'data\'] should contain key %s.' % key
+                return 'Obj[\'data\'] should contain key %s.' % key
 
         # data page contain count, num, size
         for key in ['count', 'num', 'size']:
             if key not in obj['data']['page'].keys():
-                return False, 'Obj[\'data\'][\'page\'] should contain key %s.' % key
+                return 'Obj[\'data\'][\'page\'] should contain key %s.' % key
 
         # test each archive
         for archive in obj['data']['archives']:
