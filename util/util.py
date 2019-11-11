837e1fe5fd89c2a1d7dcc310544ab2433c376dea
bunnyxt <bunnyxt@outlook.com>
2019-11-11 17:28:00 +0800

    Add ts_s_to_str(ts).

--- util/util.py
+++ util/util.py
@@ -1,7 +1,12 @@
 import datetime
+import time
 
-__all__ = ['get_ts_s']
+__all__ = ['get_ts_s', 'ts_s_to_str']
 
 
 def get_ts_s():
     return int(round(datetime.datetime.now().timestamp()))
+
+
+def ts_s_to_str(ts):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ts))
