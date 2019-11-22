32100797b3955caf715aca928ce846d1cef9ac2a
bunnyxt <bunnyxt@outlook.com>
2019-11-22 22:48:01 +0800

    Add function to print obj for debug.

--- util/util.py
+++ util/util.py
@@ -10,3 +10,8 @@ def get_ts_s():
 
 def ts_s_to_str(ts):
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ts))
+
+
+def print_obj(obj):
+    for key in dir(obj):
+        print(key, obj.__getattribute__(key))
