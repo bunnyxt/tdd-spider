55f84dc87f9887f7c1803ef082548c40b632529e
bunnyxt <bunnyxt@outlook.com>
2020-02-04 20:56:18 +0800

    Add logger_14.

--- logger/logger.py
+++ logger/logger.py
@@ -3,7 +3,7 @@ import logging.config
 import os
 
 __all__ = ['BASE_DIR', 'LOG_DIR', 'logger_01', 'logger_11', 'logger_11_c0', 'logger_11_c30',
-           'logger_12', 'logger_13', 'logger_db']
+           'logger_12', 'logger_13', 'logger_14', 'logger_db']
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 LOG_DIR = os.path.join(BASE_DIR, 'log')
@@ -182,6 +182,28 @@ LOG_CONFIG_DICT = {
             'backupCount': 30,
             'filters': ['warning_plus_filter']
         },
+        'file_info_14': {
+            'level': 'INFO',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, '14_info.log'),
+            'when': "d",
+            'interval': 1,
+            'encoding': 'utf8',
+            'backupCount': 30,
+            'filters': ['info_plus_filter']
+        },
+        'file_warning_14': {
+            'level': 'WARNING',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, '14_warning.log'),
+            'when': "d",
+            'interval': 1,
+            'encoding': 'utf8',
+            'backupCount': 30,
+            'filters': ['warning_plus_filter']
+        },
         'file_info_db': {
             'level': 'INFO',
             'formatter': 'simple',
@@ -230,6 +252,10 @@ LOG_CONFIG_DICT = {
             'handlers': ['console_info', 'file_info_13', 'file_warning_13'],
             'level': 'INFO'
         },
+        'logger_14': {
+            'handlers': ['console_info', 'file_info_14', 'file_warning_14'],
+            'level': 'INFO'
+        },
         'logger_db': {
             'handlers': ['console_info', 'file_info_db', 'file_warning_db'],
             'level': 'INFO'
@@ -245,4 +271,5 @@ logger_11_c0 = logging.getLogger('logger_11_c0')
 logger_11_c30 = logging.getLogger('logger_11_c30')
 logger_12 = logging.getLogger('logger_12')
 logger_13 = logging.getLogger('logger_13')
+logger_14 = logging.getLogger('logger_14')
 logger_db = logging.getLogger('logger_db')
