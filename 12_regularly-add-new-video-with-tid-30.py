58cf606904c56cbb227743a3de92a331b6221674
bunnyxt <bunnyxt@outlook.com>
2020-04-03 13:40:29 +0800

    Add new video via bvid.

--- 12_regularly-add-new-video-with-tid-30.py
+++ 12_regularly-add-new-video-with-tid-30.py
@@ -5,7 +5,8 @@ import math
 import time
 import schedule
 import threading
-from common import get_valid, test_archive_rank_by_partion, add_video, add_video_record_via_awesome_stat
+from common import get_valid, test_archive_rank_by_partion, add_video, add_video_via_bvid, \
+    add_video_record_via_awesome_stat
 from util import get_ts_s
 from common.error import *
 
@@ -18,8 +19,10 @@ def regularly_add_new_video():
 
     # get last added aids
     last_15_videos = DBOperation.query_last_x_video(15, session)
-    last_15_aids = list(map(lambda x: x.aid, last_15_videos))
-    logger_12.debug('Last 15 aids: %s' % last_15_aids)
+    # last_15_aids = list(map(lambda x: x.aid, last_15_videos))
+    # logger_12.debug('Last 15 aids: %s' % last_15_aids)
+    last_15_bvids = list(map(lambda x: x.bvid, last_15_videos))
+    logger_12.debug('Last 15 bvids: %s' % last_15_bvids)
 
     # get page total
     obj = bapi.get_archive_rank_by_partion(30, 1, 50)
@@ -29,8 +32,10 @@ def regularly_add_new_video():
     # add add video
     page_num = 1
     goon = True
-    last_aid_count = 0
-    added_aid_count = 0
+    # last_aid_count = 0
+    # added_aid_count = 0
+    last_bvid_count = 0
+    added_bvid_count = 0
     while page_num <= page_total and goon:
         # get obj via awesome api
         obj = get_valid(bapi.get_archive_rank_by_partion, (30, page_num, 50), test_archive_rank_by_partion)
@@ -41,26 +46,28 @@ def regularly_add_new_video():
         # process each video
         try:
             for arch in obj['data']['archives']:
-                aid = arch['aid']
+                # aid = arch['aid']
+                bvid = arch['bvid'][2:]  # remove BV prefix
 
-                # check in last aids or not
-                if last_aid_count >= 5:
-                    logger_12.debug('5 last aids meet, now break')
+                # check in last bvids or not
+                if last_bvid_count >= 5:
+                    logger_12.debug('5 last bvids meet, now break')
                     goon = False
                     break
 
-                if aid in last_15_aids:
-                    last_aid_count += 1
-                    logger_12.debug('Aid %d in last aids, count %d / 5, now continue' % (aid, last_aid_count))
+                if bvid in last_15_bvids:
+                    last_bvid_count += 1
+                    logger_12.debug('Bvid %d in last bvids, count %d / 5, now continue' % (bvid, last_bvid_count))
                     continue
 
                 # add video
                 try:
-                    new_video = add_video(aid, bapi, session)
+                    # new_video = add_video(aid, bapi, session)
+                    new_video = add_video_via_bvid(bvid, bapi, session)
                 except TddCommonError as e:
                     logger_12.warning(e)
                 else:
-                    added_aid_count += 1
+                    added_bvid_count += 1
                     logger_12.info('Add new video %s' % new_video)
                     # add stat record, which comes from awesome api
                     if 'stat' in arch.keys():
@@ -72,7 +79,7 @@ def regularly_add_new_video():
                         else:
                             logger_12.info('Add new video record %s' % new_video_record)
                     else:
-                        logger_12.warning('Fail to get stat info of video with aid %d from awesome api!' % aid)
+                        logger_12.warning('Fail to get stat info of video with bvid %d from awesome api!' % bvid)
 
         except Exception as e:
             logger_12.error('Exception caught. Detail: %s' % e)
@@ -83,7 +90,7 @@ def regularly_add_new_video():
         page_num += 1
 
     session.close()
-    logger_12.info('%d new video(s) added.' % added_aid_count)
+    logger_12.info('%d new video(s) added.' % added_bvid_count)
     logger_12.info('Finish add new video with tid 30!')
 
 
