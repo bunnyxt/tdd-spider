cdf36886af4be912f555f8a6e4f8a50b7a08813d
bunnyxt <bunnyxt@outlook.com>
2020-06-13 20:31:54 +0800

    Update pack_log_folder.sh

--- pack_log_folder.sh
+++ pack_log_folder.sh
@@ -1,12 +1,17 @@
 date_str=`date +"%Y-%m-%d"`
 file_name=log_pack_${date_str}.tar.gz
 
-echo "Now pack log folder into ${file_name} file..."
-tar -czvf ${file_name} log
+echo "Now move log/*.log.* file into log_tmp folder..."
+mkdir log_tmp
+mv log/*.log.* log_tmp
+echo "Finish move log/*.log.* file into log_tmp folder!"
+
+echo "Now pack log_tmp folder into ${file_name} file..."
+tar -czvf ${file_name} log_tmp
 echo "Finish pack log folder into ${file_name} file!"
 
-rm log/*
-echo "Finish clear log folder!"
+rm -r log_tmp
+echo "Finish remove log_tmp folder!"
 
 mv ${file_name} /var/www/res/tdd/log_pack/
 echo "File moved to res folder!"
