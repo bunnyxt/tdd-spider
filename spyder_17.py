ae1b1b563233871ab9f79bd5f9e41abd7a47f287
bunnyxt <bunnyxt@outlook.com>
2020-09-14 13:42:27 +0800

    Fix process logic, seperate whole loop into main fetch and residual fetch.

--- spyder_17.py
+++ spyder_17.py
@@ -1,51 +1,88 @@
 from spider import WebSpider
 from spider.custom import ApiFetcher, TddMemberFollowerRecordParser, DbSaver, LocalProxieser
 import logging
-from db import Session
+from db import DBOperation, Session
 from util import get_ts_s, ts_s_to_str
+# from spider.custom import FileSaver
 
 
-def create_web_spider():
-    return WebSpider(fetcher=ApiFetcher(sleep_time=0, max_repeat=10),
-                     parser=TddMemberFollowerRecordParser(),
-                     saver=DbSaver(get_session=Session),
-                     proxieser=LocalProxieser(sleep_time=5, proxy_num=100),
-                     queue_parse_size=200, queue_proxies_size=500)
+def create_web_spider(sequential=False):
+    if sequential:
+        # for residual fetch
+        # no proxieser, longer fetcher sleep time, fewer fetch max repeat
+        web_spider = WebSpider(fetcher=ApiFetcher(sleep_time=3, max_repeat=3),
+                               parser=TddMemberFollowerRecordParser(),
+                               saver=DbSaver(get_session=Session),
+                               # saver=FileSaver(filename='17_save.log'),
+                               queue_parse_size=200)
+    else:
+        # for main fetch
+        web_spider = WebSpider(fetcher=ApiFetcher(sleep_time=0, max_repeat=10),
+                               parser=TddMemberFollowerRecordParser(),
+                               saver=DbSaver(get_session=Session),
+                               # saver=FileSaver(filename='17_save.log'),
+                               proxieser=LocalProxieser(sleep_time=5, proxy_num=100),
+                               queue_parse_size=200, queue_proxies_size=500)
+    return web_spider
 
 
 def spider_17():
     start_ts = get_ts_s()
-    web_spider = create_web_spider()
 
     # load all mids
     session = Session()
-    result = session.execute('select m.mid from tdd_member m join tdd_member_follower_record r '
-                             'on m.last_follower = r.id order by r.follower desc')
-    mids = [r[0] for r in result]
+    mids = DBOperation.query_all_member_mids(session=session)
     session.close()
-    # add urls to fetch queue
-    for mid in mids:
-        url = 'http://api.bilibili.com/x/relation/stat?vmid={0}'.format(mid)
-        web_spider.put_item_to_queue_fetch(1, url, {}, 0, 0)
 
+    # init urls
+    left_url_list = []
+    for mid in mids:
+        left_url_list.append('http://api.bilibili.com/x/relation/stat?vmid={0}'.format(mid))
+
+    # main fetch, multithreading spider
     spyder_round = 1
-    while True:
+    left_url_num = len(left_url_list)
+    max_spyder_round = 5  # at most 5 round
+    least_left_url_num = 100  # at least 100 urls left to fetch
+    while spyder_round <= max_spyder_round and left_url_num >= least_left_url_num:
+        # create web spider
+        web_spider = create_web_spider()
+
+        # init add urls
+        for url in left_url_list:
+            web_spider.put_item_to_queue_fetch(1, url, {}, 0, 0)
+
+        # launch web spider
+        logging.warning('spyder round %d start, url total %d' % (spyder_round, left_url_num))
         web_spider.start_working(fetcher_num=50)
         web_spider.wait_for_finished()
 
-        fetcher_fail_url_list = web_spider.get_fetcher_fail_url_list()
-        if len(fetcher_fail_url_list) > 0:
-            # go continue
-            logging.warning('round %d done, url left %d' % (spyder_round, len(fetcher_fail_url_list)))
-            spyder_round += 1
+        # update left urls
+        left_url_list = web_spider.get_fail_url_list()
+        left_url_num = len(left_url_list)
+        logging.warning('spyder round %d done, url left %d' % (spyder_round, left_url_num))
+        spyder_round += 1
 
-            web_spider = create_web_spider()
+    # residual fetch, sequential spider
+    if left_url_num > 0:
+        # create web spider
+        web_spider = create_web_spider(sequential=True)
 
-            for url in fetcher_fail_url_list:
-                web_spider.put_item_to_queue_fetch(1, url, {}, 0, 0)
-        else:
-            break
-    logging.warning('round %d done, all finished' % spyder_round)
+        # init add urls
+        for url in left_url_list:
+            web_spider.put_item_to_queue_fetch(1, url, {}, 0, 0)
+
+        # launch web spider
+        logging.warning('residual spyder start, url total %d' % left_url_num)
+        web_spider.start_working(fetcher_num=1)  # one fetcher only
+        web_spider.wait_for_finished()
+
+        # update left urls
+        left_url_list = web_spider.get_fail_url_list()
+        left_url_num = len(left_url_list)
+        logging.warning('residual spyder done, url left %d' % left_url_num)
+        if left_url_num > 0:
+            logging.warning('left url list: %s' % left_url_list)
 
     end_ts = get_ts_s()
     logging.warning('start time %s' % ts_s_to_str(start_ts))
