5787c7c36848a8c28788b1a83af656c3593e12ae
bunnyxt <bunnyxt@outlook.com>
2020-11-02 13:52:05 +0800

    Fix bug, find attribute in keys.

--- common/operation.py
+++ common/operation.py
@@ -398,7 +398,7 @@ def update_video_via_bvid(bvid, bapi, session):
             if view_obj['data']['owner']['mid'] != old_obj.mid:
                 video_update_logs.append(TddVideoLog(added, aid, bvid, 'mid', old_obj.mid, view_obj['data']['owner']['mid']))
                 old_obj.mid = view_obj['data']['owner']['mid']
-            if 'attribute' in view_obj['data']['attribute']:
+            if 'attribute' in view_obj['data'].keys():
                 if view_obj['data']['attribute'] != old_obj.attribute:
                     video_update_logs.append(TddVideoLog(added, aid, bvid, 'attribute', old_obj.attribute, view_obj['data']['attribute']))
                     old_obj.attribute = view_obj['data']['attribute']
