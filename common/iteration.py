95cff9dbe2681af1f37f3ff44956945c3a47c6f5
bunnyxt <bunnyxt@outlook.com>
2020-11-03 14:49:33 +0800

    Add context and start_page_number parameter.

--- common/iteration.py
+++ common/iteration.py
@@ -8,14 +8,18 @@ import time
 __all__ = ['iter_get_archive_rank_by_partion']
 
 
-def iter_get_archive_rank_by_partion(tid, iter_func, colddown=0.1):
+def iter_get_archive_rank_by_partion(tid, iter_func, context, start_page_num=1, colddown=0):
     """
-
+    iter get archive rank by partion api
     :param tid: api param, category id
-    :param iter_func: function to process each iter item, format: iter_func(iter_item, **iter_context)
+    :param iter_func: function to process each iter item, format: iter_func(iter_item, context, **iter_context)
+    :param context: context passed into iter_func
+    :param start_page_num: iter loop start page num
     :param colddown: api request interval colddown
     :return:
     """
+    logging.info('Now iter get archive rank by partion api...')
+
     bapi = BiliApi()
 
     last_page_aids = []  # aids occurred in last page
@@ -24,30 +28,36 @@ def iter_get_archive_rank_by_partion(tid, iter_func, colddown=0.1):
     # get page total
     page_obj = bapi.get_archive_rank_by_partion(tid, 1, 50)
     page_total = math.ceil(page_obj['data']['page']['count'] / 50)
-    logging.info('%d page(s) found' % page_total)
+    logging.info('Found %d page(s) in total' % page_total)
 
     # iter loop
-    page_num = 1
+    page_num = start_page_num
     while page_num <= page_total:
         try:
             # get page_obj via get_archive_rank_by_partion api
             page_obj = get_valid(bapi.get_archive_rank_by_partion, (30, page_num, 50), test_archive_rank_by_partion)
             if page_obj is None:
-                logging.warning('Page num %d fail! Cannot get valid obj.' % page_num)
+                logging.warning('Page num %d fail! Cannot get valid obj' % page_num)
                 page_num += 1
                 continue
 
             added = get_ts_s()  # api response ts (approximate)
+
+            # update page total
+            page_total_new = math.ceil(page_obj['data']['page']['count'] / 50)
+            if page_total_new > page_total:
+                logging.info('Update page total num from %d to %d' % (page_total, page_total_new))
+                page_total = page_total_new
 
             # page_obj items iter loop
             for iter_item in page_obj['data']['archives']:
                 aid = iter_item['aid']
                 if aid in last_page_aids:
                     # aid occurred in last page, skip
-                    logging.warning('Aid %d occurred in last page (page_num = %d).' % (aid, page_num - 1))
+                    logging.info('Aid %d occurred in last page (page_num = %d)' % (aid, page_num - 1))
                     continue
 
-                iter_func(iter_item, added=added)  # iter_item, **iter_context
+                iter_func(iter_item, context, added=added)  # iter_item, context, **iter_context
 
                 this_page_aids.append(aid)
 
@@ -55,11 +65,15 @@ def iter_get_archive_rank_by_partion(tid, iter_func, colddown=0.1):
             last_page_aids = this_page_aids
             this_page_aids = []
 
+            logging.debug('Awesome api page %d / %d done' % (page_num, page_total))
             if page_num % 100 == 0:
-                logging.info('Awesome api fetch %d / %d done' % (page_num, page_total))
+                logging.info('Awesome api page %d / %d done' % (page_num, page_total))
         except Exception as e:
-            logging.error('Awesome api fetch %d / %d error, Exception caught. Detail: %s' % (page_num, page_total, e))
+            logging.error('Awesome api page %d / %d error, Exception caught. Detail: %s' % (page_num, page_total, e))
         finally:
             page_num += 1
             time.sleep(colddown)
-    logging.info('Awesome api fetch %d / %d done' % (page_num - 1, page_total))
+
+    if (page_num - 1) % 100 != 0:
+        logging.info('Awesome api fetch %d / %d done' % (page_num - 1, page_total))
+    logging.info('Finish iter get archive rank by partion api')
