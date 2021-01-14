b4020b567b9ef3be4d5cff58d92cbda2a4bf523c
bunnyxt <bunnyxt@outlook.com>
2021-01-15 00:32:35 +0800

    Move ua_list to instance property.

--- spider/custom/fetcher.py
+++ spider/custom/fetcher.py
@@ -4,20 +4,9 @@ import random
 
 
 class ApiFetcher(Fetcher):
-    def random_ua(self):
-        # ua_list = [
-        #     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
-        #     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
-        #     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-        #     'Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50',
-        #     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)',
-        #     'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)',
-        #     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E) QQBrowser/6.9.11079.201',
-        #     'User-Agent,Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4094.1 Safari/537.36',
-        #     'User-Agent,Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15',
-        #     'User-Agent, MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
-        # ]
-        ua_list = [
+    def __init__(self):
+        super().__init__()
+        self._ua_list = [
             # PC Browser
             # Google,win
             r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
@@ -58,11 +47,13 @@ class ApiFetcher(Fetcher):
             # Android N1
             r'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
         ]
-        return random.choice(ua_list)
+
+    def _get_random_ua(self):
+        return random.choice(self._ua_list)
 
     def url_fetch(self, priority: int, url: str, keys: dict, deep: int, repeat: int, proxies=None):
         response = requests.get(url, proxies=proxies, verify=False, allow_redirects=False,
-                                headers={'User-Agent': self.random_ua()}, timeout=(3.05, 10))
+                                headers={'User-Agent': self._get_random_ua()}, timeout=(3.05, 10))
         response.raise_for_status()
         if response.headers['Content-Type'].find('application/json') == -1:
             raise RuntimeError('response should have application/json MIME type')
