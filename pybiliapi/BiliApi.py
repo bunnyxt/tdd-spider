688e8125387dd294b8ed1415b157305b2d57cf72
bunnyxt <bunnyxt@outlook.com>
2023-01-26 11:02:17 -0800

    feat: add random user agent when send request

--- pybiliapi/BiliApi.py
+++ pybiliapi/BiliApi.py
@@ -1,5 +1,6 @@
 import urllib3
 import json
+import random
 import logging
 logger = logging.getLogger('BiliApi')
 
@@ -31,6 +32,51 @@ class BiliApi:
             self.use_proxy = False
             self.timeout = 5
             self.retries = 3
+        self._ua_list = [
+            # PC Browser
+            # Google,win
+            r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+            # Google,mac
+            r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+            # Google,linux
+            r'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+            # Opera,win
+            r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31',
+            # Opera,mac
+            r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31',
+            # Firefox,win
+            r'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
+            # Firefox,mac
+            r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0',
+            # Safari,mac
+            r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A',
+            # 360 browser
+            r'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+            # Sogou browser
+            r'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+            # UC browser
+            r'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4094.1 Safari/537.36',
+            # Internet Explorer 8
+            r'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+            # Internet Explorer 9
+            r'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+
+            # Mobile Browser
+            # Android QQ browser For android
+            r'MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+            # Android Opera Mobile
+            r'Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10',
+            # BlackBerry
+            r'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+',
+            # Nokia N97
+            r'Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-1/20.0.019; Profile/MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/525 (KHTML, like Gecko) BrowserNG/7.1.18124',
+            # Android N1
+            r'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
+        ]
+
+
+    def _get_random_ua(self):
+        return random.choice(self._ua_list)
 
     def _get_proxy_url(self, https=False):
         if not self.use_proxy:
@@ -69,7 +115,7 @@ class BiliApi:
 
                 # go request
                 try:
-                    response = http.request(method, url, timeout=self.timeout, retries=self.retries)
+                    response = http.request(method, url, timeout=self.timeout, retries=self.retries, headers={'User-Agent': self._get_random_ua()})
                     if response.status != 200:
                         continue
                     html = response.data.decode()
@@ -86,7 +132,7 @@ class BiliApi:
             return None
         else:
             try:
-                response = self.http.request(method, url, timeout=self.timeout, retries=self.retries)
+                response = self.http.request(method, url, timeout=self.timeout, retries=self.retries, headers={'User-Agent': self._get_random_ua()})
                 if response.status != 200:
                     logger.warning('Fail to get response with status code %d.' % response.status)
                     return None
