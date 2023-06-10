e16ab7afb61f2f6b90c5be11a091dfddbe5d7d87
bunnyxt <bunnyxt@outlook.com>
2023-06-09 17:16:33 -0700

    chore: remove kdl related config

--- conf/conf.py
+++ conf/conf.py
@@ -3,7 +3,7 @@ import os
 
 __all__ = ['CONFIG_PATH', 'CONFIG',
            'get_db_args', 'get_library_cookie', 'get_bilibili_cookie', 'get_sckey',
-           'get_proxy_pool_url', 'get_kdl_order_id', 'get_kdl_apikey']
+           'get_proxy_pool_url']
 
 # use config parser to load config
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'conf.ini')
@@ -29,11 +29,3 @@ def get_sckey():
 
 def get_proxy_pool_url():
     return CONFIG.get('proxy', 'proxy_pool_url')
-
-
-def get_kdl_order_id():
-    return CONFIG.get('proxy', 'kdl_order_id')
-
-
-def get_kdl_apikey():
-    return CONFIG.get('proxy', 'kdl_apikey')
