ec8a55b085e4cb19574096b9cb14c6526f80fd1b
bunnyxt <bunnyxt@outlook.com>
2023-06-11 13:18:28 -0700

    feat: refactor using update_member task

--- 61_update-member-info-of-name-duplicated-member.py
+++ 61_update-member-info-of-name-duplicated-member.py
@@ -1,17 +1,23 @@
 import logging
 from logutils import logging_init
 from db import Session
-from pybiliapi import BiliApi
-from common import update_member, TddCommonError
+from service import Service
+from task import update_member
+from common.error import TddError
+from collections import defaultdict
+from util import get_ts_s, ts_s_to_str, format_ts_s, format_ts_ms, get_ts_ms
+from serverchan import sc_send
 
 logger = logging.getLogger('61')
 
 
 def update_member_info_of_name_duplicated_member():
     logger.info('Now start update member info of name duplicated member...')
+    start_ts = get_ts_s()  # get start ts
 
     session = Session()
-    bapi = BiliApi()
+    service = Service(mode="worker", retry=20)
+    statistics = defaultdict(int)
 
     logger.info('Now get duplicated names...')
     results = session.execute(
@@ -20,12 +26,12 @@ def update_member_info_of_name_duplicated_member():
     )
     duplicated_name_list = []
     for r in results:
-        logger.info('%d members have duplicated name %s' % (r[1], r[0]))
+        logger.info(f'{r[1]} members have duplicated name {r[0]}')
         duplicated_name_list.append(r[0])
     if len(duplicated_name_list) == 0:
         logger.info('No duplicated name detected!')
     else:
-        logger.info('%d duplicated names detected!' % len(duplicated_name_list))
+        logger.info(f'{len(duplicated_name_list)} duplicated names detected!')
 
         logger.info('Now get member mids with duplicated name...')
         results = session.execute('select mid from tdd_member where `name` in (%s);'
@@ -33,25 +39,56 @@ def update_member_info_of_name_duplicated_member():
         name_duplicated_mids = []
         for r in results:
             name_duplicated_mids.append(r[0])
-        logger.info('%d name duplicated mids got!' % len(name_duplicated_mids))
+        logger.info(f'{len(name_duplicated_mids)} name duplicated mids got!')
 
-        for i, mid in enumerate(name_duplicated_mids, 1):
+        for idx, mid in enumerate(name_duplicated_mids, 1):
+            start_ts_ms = get_ts_ms()
             try:
-                tdd_member_logs = update_member(mid, bapi, session)
-            except TddCommonError as e:
-                logger.error('Fail to update member info mid %d, TddCommonError Detail: %s' % (mid, e))
+                tdd_member_logs = update_member(mid, service, session)
+            except TddError as e:
+                logger.warning(f'Fail to update member info. mid: {mid}, error: {e}')
+                statistics['tdd_error_count'] += 1
             except Exception as e:
-                logger.error('Fail to update member info mid %d, Exception Detail: %s' % (mid, e))
+                logger.warning(f'Fail to update member info. mid: {mid}, error: {e}')
+                statistics['other_exception_count'] += 1
             else:
                 if len(tdd_member_logs) == 0:
-                    logger.info('Member mid %d info not changed' % mid)
+                    statistics['no_update_count'] += 1
                 else:
-                    for log in tdd_member_logs:
-                        logger.info('Member mid %d attribute %s changed from %s to %s' % (
-                            log.mid, log.attr, log.oldval, log.newval))
-                logger.debug('Finish update member info mid %d' % mid)
+                    statistics['change_count'] += 1
+                for log in tdd_member_logs:
+                    logger.info(f'Update member info. mid: {mid}, attr: {log.attr}, {log.oldval} -> {log.newval}')
+                    statistics['change_log_count'] += 1
+                logger.debug(f'{tdd_member_logs} log(s) found. mid: {mid}')
+            end_ts_ms = get_ts_ms()
+            cost_ms = end_ts_ms - start_ts_ms
+            logger.debug(f'Finish update member info. mid: {mid}, cost: {format_ts_ms(cost_ms)}')
+            statistics['total_count'] += 1
+            statistics['total_cost_ms'] += cost_ms
+
+    # get end ts
+    end_ts = get_ts_s()
+
+    # make summary
+    summary = \
+        'update member info of name duplicated name done!\n\n' \
+        f'start: {ts_s_to_str(start_ts)}, ' \
+        f'end: {ts_s_to_str(end_ts)}, ' \
+        f'cost: {format_ts_s(end_ts - start_ts)}\n\n' \
+        f'total count: {statistics["total_count"]}, ' + \
+        f'average cost per service: {format_ts_ms(statistics["total_cost_ms"] // statistics["total_count"])}\n\n' \
+        f'tdd error count: {statistics["tdd_error_count"]}\n\n' \
+        f'other exception count: {statistics["other_exception_count"]}\n\n' \
+        f'no update count: {statistics["no_update_count"]}\n\n' \
+        f'change count: {statistics["change_count"]}\n\n' \
+        f'change log count: {statistics["change_log_count"]}\n\n' \
+        f'by.bunnyxt, {ts_s_to_str(get_ts_s())}'
 
     logger.info('Finish update member info of name duplicated member!')
+    logger.warning(summary)
+
+    # send sc
+    sc_send('Finish update member info of name duplicated name!', summary)
 
     session.close()
 
