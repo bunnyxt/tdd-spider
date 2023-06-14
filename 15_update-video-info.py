089441cec0661e9c6076713b912bd6fca6c0639d
bunnyxt <bunnyxt@outlook.com>
2023-06-14 14:22:38 -0700

    feat: use multi thread to boost

--- 15_update-video-info.py
+++ 15_update-video-info.py
@@ -4,11 +4,54 @@ from common.error import TddError
 from task import update_video
 from serverchan import sc_send
 from util import get_ts_s, ts_s_to_str, get_week_day, b2a, format_ts_s, get_ts_ms, format_ts_ms
+from threading import Thread
+from queue import Queue
 from collections import defaultdict
 from logutils import logging_init
 import logging
 
 logger = logging.getLogger('15')
+
+
+class UpdateVideoServiceRunner(Thread):
+    def __init__(self, name, bvid_queue, statistics, service):
+        super().__init__()
+        self.name = name
+        self.bvid_queue = bvid_queue
+        self.statistics = statistics
+        self.service = service
+        self.session = Session()
+        self.logger = logging.getLogger(f'UpdateVideoServiceRunner.{self.name}')
+
+    def run(self):
+        self.logger.info(f'Runner start.')
+        while not self.bvid_queue.empty():
+            bvid = self.bvid_queue.get()
+            start_ts_ms = get_ts_ms()
+            try:
+                tdd_video_logs = update_video(b2a(bvid), self.service, self.session)
+            except TddError as e:
+                logger.warning(f'Fail to update video info. bvid: {bvid}, error: {e}')
+                self.statistics['tdd_error_count'] += 1
+            except Exception as e:
+                logger.warning(f'Fail to update video info. bvid: {bvid}, error: {e}')
+                self.statistics['other_exception_count'] += 1
+            else:
+                if len(tdd_video_logs) == 0:
+                    self.statistics['no_update_count'] += 1
+                else:
+                    self.statistics['change_count'] += 1
+                for log in tdd_video_logs:
+                    logger.info(f'Update video info. bvid: {bvid}, attr: {log.attr}, {log.oldval} -> {log.newval}')
+                    self.statistics['change_log_count'] += 1
+                logger.debug(f'{tdd_video_logs} log(s) found. bvid: {bvid}')
+            end_ts_ms = get_ts_ms()
+            cost_ms = end_ts_ms - start_ts_ms
+            self.logger.debug(f'Finish update video info. bvid: {bvid}, cost: {format_ts_ms(cost_ms)}')
+            self.statistics['total_count'] += 1
+            self.statistics['total_cost_ms'] += cost_ms
+        self.session.close()
+        self.logger.info(f'Runner end.')
 
 
 def update_video_info():
@@ -35,38 +78,30 @@ def update_video_info():
 
     logger.info(f'Will update {len(bvids)} videos info.')
 
-    total_count = len(bvids)
+    # put mid into queue
+    bvid_queue = Queue()
+    for bvid in bvids:
+        bvid_queue.put(bvid)
+    logger.info(f'{bvid_queue.qsize()} bvids put into queue.')
 
     # prepare statistics
     statistics = defaultdict(int)
 
-    for idx, bvid in enumerate(bvids, 1):
-        start_ts_ms = get_ts_ms()
-        try:
-            tdd_video_logs = update_video(b2a(bvid), service, session)
-        except TddError as e:
-            logger.warning(f'Fail to update video info. bvid: {bvid}, error: {e}')
-            statistics['tdd_error_count'] += 1
-        except Exception as e:
-            logger.warning(f'Fail to update video info. bvid: {bvid}, error: {e}')
-            statistics['other_exception_count'] += 1
-        else:
-            if len(tdd_video_logs) == 0:
-                statistics['no_update_count'] += 1
-            else:
-                statistics['change_count'] += 1
-            for log in tdd_video_logs:
-                logger.info(f'Update video info. bvid: {bvid}, attr: {log.attr}, {log.oldval} -> {log.newval}')
-                statistics['change_log_count'] += 1
-            logger.debug(f'{tdd_video_logs} log(s) found. bvid: {bvid}')
-        end_ts_ms = get_ts_ms()
-        cost_ms = end_ts_ms - start_ts_ms
-        logger.debug(f'Finish update video info. bvid: {bvid}, cost: {format_ts_ms(cost_ms)}')
-        statistics['total_count'] += 1
-        statistics['total_cost_ms'] += cost_ms
-        if idx % 1000 == 0:
-            logger.info(f'{idx} / {total_count} done')
-    logger.info(f'{total_count} / {total_count} done')
+    # create service runner
+    service_runner_num = 20
+    service_runner_list = []
+    for i in range(service_runner_num):
+        service_runner = UpdateVideoServiceRunner(f'runner_{i}', bvid_queue, statistics, service)
+        service_runner_list.append(service_runner)
+
+    # start service runner
+    for service_runner in service_runner_list:
+        service_runner.start()
+    logger.info(f'{service_runner_num} service runner started.')
+
+    # wait for service runner
+    for service_runner in service_runner_list:
+        service_runner.join()
 
     # get end ts
     end_ts = get_ts_s()
