eba01ba8885fe10c4ecc71e9bc1d44fc5e2da9ac
bunnyxt <bunnyxt@outlook.com>
2023-06-18 13:04:41 -0700

    feat: refactor using task and service

--- 12_add-new-video-with-tid-30.py
+++ 12_add-new-video-with-tid-30.py
@@ -1,11 +1,12 @@
-from pybiliapi import BiliApi
+from service import Service
+from serverchan import sc_send
+from task import add_video, commit_video_record_via_archive_stat, AlreadyExistError
+from collections import defaultdict
 from db import Session
-from common import get_valid, test_archive_rank_by_partion, add_video_via_bvid, add_video_record_via_awesome_stat, \
-    AlreadyExistError, TddCommonError
-from util import get_ts_s
-from conf import get_proxy_pool_url
+from util import get_ts_s, ts_s_to_str, format_ts_s
 from logutils import logging_init
 import logging
+
 logger = logging.getLogger('12')
 
 
@@ -14,63 +15,70 @@ def add_new_video_with_tid_30():
     start_ts = get_ts_s()  # get start ts
 
     session = Session()
+    service = Service(mode='worker')
 
-    bapi = BiliApi()
-    # check whether awesome api is accessible (no 412) via local ip
-    test_page_obj = get_valid(bapi.get_archive_rank_by_partion, (30, 1, 50), test_archive_rank_by_partion)
-    if test_page_obj is None:
-        # use bapi with proxy pool
-        bapi = BiliApi(get_proxy_pool_url())
-        logger.warning('Fail to get valid test page obj, use bapi with proxy pool.')
+    # prepare statistics
+    statistics: defaultdict[str, int] = defaultdict(int)
 
-    # add add video
+    # add new video
     page_num = 1
-    added_bvid_count = 0
     while page_num <= 3:  # check latest 3 page
-        # get page_obj via awesome api
-        page_obj = get_valid(bapi.get_archive_rank_by_partion, (30, page_num, 50), test_archive_rank_by_partion)
-        if page_obj is None:
-            logger.warning('Fail to get valid obj with page_num %d, continue to next page' % page_num)
-            page_num += 1
-        # process each video
+        # get archive rank by partion
         try:
-            for arch in page_obj['data']['archives']:
-                bvid = arch['bvid'][2:]  # remove BV prefix
-                # add video
-                try:
-                    new_video = add_video_via_bvid(bvid, bapi, session)
-                except AlreadyExistError as e:
-                    # video already added, completely common
-                    # logger.debug('AlreadyExistError detected when add video bvid %s! Detail: %s' % (bvid, e))
-                    pass
-                except TddCommonError as e:
-                    logger.warning('TddCommonError detected when add video bvid %s! Detail: %s' % (bvid, e))
-                except Exception as e:
-                    logger.warning('Exception caught when add video bvid %s! Detail: %s' % (bvid, e))
-                else:
-                    added_bvid_count += 1
-                    logger.info('Add new video %s' % new_video)
-                    # add stat record, which comes from awesome api
-                    if 'stat' in arch.keys():
-                        stat = arch['stat']
-                        try:
-                            new_video_record = add_video_record_via_awesome_stat(get_ts_s(), stat, session)
-                        except TddCommonError as e:
-                            logger.warning('TddCommonError detected when add new video record of video bvid %s! Detail: %s' % (bvid, e))
-                        else:
-                            logger.info('Add new video record %s' % new_video_record)
-                    else:
-                        logger.warning('Fail to get stat info of video with bvid %s from awesome api!' % bvid)
+            archive_rank_by_partion = service.get_archive_rank_by_partion({'tid': 30, 'pn': page_num, 'ps': 50})
         except Exception as e:
-            logger.error('Exception caught when traverse page_obj[\'data\'][\'archives\']. Detail: %s' % e)
-        # update page num
-        # logger.debug('Page %d done.' % page_num)
+            logger.error(f'Fail to get archive rank by partion. tid: 30, pn: {page_num}, ps: 50, error: {e}')
+            statistics['other_exception_count'] += 1
+            page_num += 1
+            continue
+
+        for archive in archive_rank_by_partion.archives:
+            # add video
+            try:
+                new_video = add_video(archive.aid, service, session)
+            except AlreadyExistError:
+                logger.debug(f'Video parsed from archive already exist! archive: {archive}')
+            except Exception as e:
+                logger.warning(f'Fail to add video parsed from archive! archive: {archive}, error: {e}')
+                statistics['other_exception_count'] += 1
+            else:
+                logger.info(f'New video parsed from archive added! video: {new_video}')
+                statistics['total_count'] += 1
+                # commit video record via archive stat
+                try:
+                    new_video_record = commit_video_record_via_archive_stat(archive.stat, new_video.aid)
+                except Exception as e:
+                    logger.warning(f'Fail to add video record parsed from archive stat! '
+                                   f'archive: {archive}, error: {e}')
+                    statistics['other_exception_count'] += 1
+                else:
+                    logger.info(f'New video record parsed from archive stat committed! '
+                                f'video record: {new_video_record}')
+
+        logger.debug(f'Archive page {page_num} done.')
         page_num += 1
 
-    session.close()
+    # get end ts
     end_ts = get_ts_s()
-    logger.info('%d new video(s) added.' % added_bvid_count)
-    logger.info('Finish add new video with tid 30! Time usage: %ds' % (end_ts - start_ts))
+
+    # make summary
+    summary = \
+        'add new video with tid 30 done!\n\n' \
+        f'start: {ts_s_to_str(start_ts)}, ' \
+        f'end: {ts_s_to_str(end_ts)}, ' \
+        f'cost: {format_ts_s(end_ts - start_ts)}\n\n' \
+        f'total count: {statistics["total_count"]}\n\n' + \
+        f'other exception count: {statistics["other_exception_count"]}\n\n' \
+        f'by.bunnyxt, {ts_s_to_str(get_ts_s())}'
+
+    logger.info('Finish add new video with tid 30!')
+    logger.warning(summary)
+
+    # send sc
+    if statistics["other_exception_count"] > 0:
+        sc_send('Finish add new video with tid 30!', summary)
+
+    session.close()
 
 
 def main():
