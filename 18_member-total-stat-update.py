c75b976c98d08a2d778c66bb05b1ac0f490d7ab2
bunnyxt <bunnyxt@outlook.com>
2023-06-19 23:34:20 -0700

    refactor: clean up process

--- 18_member-total-stat-update.py
+++ 18_member-total-stat-update.py
@@ -1,27 +1,35 @@
 import logging
 from logutils import logging_init
 from db import Session, TddMemberTotalStatRecord
-from util import get_ts_s, ts_s_to_str
+from util import get_ts_s, ts_s_to_str, format_ts_s
 from serverchan import sc_send
+
 logger = logging.getLogger('18')
 
 
 def member_total_stat_update():
     logger.info('Now start member total stat update...')
+    start_ts = get_ts_s()  # get start ts
 
     session = Session()
 
     try:
-        start_ts = get_ts_s()  # get start ts
         added = start_ts
 
-        sql = 'select v.aid, v.mid as v_mid, r.view, r.danmaku, r.reply, r.favorite, r.coin, r.share, r.like, s.mid as s_mid ' + \
-              'from tdd_video v left join tdd_video_record r on v.laststat = r.id left join tdd_video_staff s on v.aid = s.aid ' + \
-              'where v.mid is not null && v.laststat is not null;'
+        sql = 'select ' \
+              'v.aid, v.mid as v_mid, ' \
+              'r.view, r.danmaku, r.reply, r.favorite, r.coin, r.share, r.like, ' \
+              's.mid as s_mid ' \
+              'from ' \
+              'tdd_video v ' \
+              'left join tdd_video_record r on v.laststat = r.id ' \
+              'left join tdd_video_staff s on v.aid = s.aid ' \
+              'where ' \
+              'v.mid is not null && v.laststat is not null;'
         result = session.execute(sql)
         result = [[r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9]] for r in result]
         result_len = len(result)
-        logger.info('%d result got' % result_len)
+        logger.info(f'Total {result_len} result got.')
 
         mid_dict = {}
         for r in result:
@@ -36,35 +44,42 @@ def member_total_stat_update():
             mid_dict[mid].coin += r[6]
             mid_dict[mid].share += r[7]
             mid_dict[mid].like += r[8]
-
-        count = 0
         mid_dict_len = len(mid_dict)
+        logger.info(f'Total {mid_dict_len} items in mid_dict created.')
+
+        cnt = 0
         for v in mid_dict.values():
             session.add(v)
-            count += 1
-            if count % 100 == 0:
+            cnt += 1
+            if cnt % 100 == 0:
                 session.commit()
-                logger.info('%d / %d added' % (count, mid_dict_len))
-
-        finish_ts = get_ts_s()
-
-        # make summary
-        summary = \
-            '18 member total stat update done\n\n' + \
-            'start: %s, finish: %s, timespan: %ss\n\n' \
-            % (ts_s_to_str(start_ts), ts_s_to_str(finish_ts), (finish_ts - start_ts)) + \
-            'result len: %d\n\n' % result_len + \
-            'mid dict len: %d\n\n' % mid_dict_len + \
-            'by.bunnyxt, %s' % ts_s_to_str(get_ts_s())
-
-        logger.info('Finish member total stat update!')
-
-        logger.info(summary)
-
-        # send sc
-        sc_send('Finish member total stat update!', summary)
+                logger.info(f'{cnt} / {mid_dict_len} added')
+        if cnt % 100 != 0:
+            session.commit()
+            logger.info(f'{cnt} / {mid_dict_len} added')
     except Exception as e:
-        logger.exception(e)
+        logger.critical(f'Exception occurred when updating member total stat! error: {e}')
+        session.rollback()
+        exit(1)
+
+    # get end ts
+    end_ts = get_ts_s()
+
+    # make summary
+    summary = \
+        'member total stat update done!\n\n' \
+        f'start: {ts_s_to_str(start_ts)}, ' \
+        f'end: {ts_s_to_str(end_ts)}, ' \
+        f'cost: {format_ts_s(end_ts - start_ts)}\n\n' \
+        f'result len: {result_len}\n\n' \
+        f'mid dict len: {mid_dict_len}\n\n' \
+        f'by.bunnyxt, {ts_s_to_str(get_ts_s())}'
+
+    logger.info('Finish member total stat update!')
+    logger.warning(summary)
+
+    # send sc
+    sc_send('Finish member total stat update!', summary)
 
     session.close()
 
