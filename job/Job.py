ec1ee3d0199bd243ce37fe44cc12944649bb3612
bunnyxt <bunnyxt@outlook.com>
2023-07-08 14:50:30 -0700

    feat: use JobStat instead of defaultdict stat

--- job/Job.py
+++ job/Job.py
@@ -1,4 +1,6 @@
 from threading import Thread
+from .JobStat import JobStat
+from util import get_ts_ms
 import logging
 
 logger = logging.getLogger('Job')
@@ -10,8 +12,22 @@ class Job(Thread):
     def __init__(self, name: str):
         super().__init__()
         self.name = name
+        self.stat = JobStat()
         self.logger = logging.getLogger(f'{self.__class__.__name__}.{self.name}')
 
     def run(self):
-        self.logger.info(f'Job start.')
-        self.logger.info(f'Job end.')
+        self.logger.info('Job start.')
+        self.stat.start_ts_ms = get_ts_ms()
+
+        self.process()
+
+        self.stat.end_ts_ms = get_ts_ms()
+        self.logger.info('Job end.')
+
+        self.cleanup()
+
+    def process(self):
+        self.logger.info('Override this method for customizing job process.')
+
+    def cleanup(self):
+        self.logger.info('Override this method for customizing clean up, e.g. close session.')
--- job/UpdateVideoJob.py
+++ job/UpdateVideoJob.py
@@ -10,36 +10,34 @@ __all__ = ['UpdateVideoJob']
 
 
 class UpdateVideoJob(Job):
-    def __init__(self, name: str, bvid_queue: Queue[str], statistics: defaultdict[str, int], service: Service):
+    def __init__(self, name: str, bvid_queue: Queue[str], service: Service):
         super().__init__(name)
         self.bvid_queue = bvid_queue
-        self.statistics = statistics
         self.service = service
         self.session = Session()
 
-    def run(self):
-        self.logger.info(f'Job start.')
+    def process(self):
         while not self.bvid_queue.empty():
             bvid = self.bvid_queue.get()
+            self.logger.debug(f'Now start update video info. bvid: {bvid}')
             start_ts_ms = get_ts_ms()
+
             try:
                 tdd_video_logs = update_video(b2a(bvid), self.service, self.session)
             except Exception as e:
                 self.logger.error(f'Fail to update video info. bvid: {bvid}, error: {e}')
-                self.statistics['other_exception_count'] += 1
+                self.stat.condition['exception'] += 1
             else:
-                if len(tdd_video_logs) == 0:
-                    self.statistics['no_update_count'] += 1
-                else:
-                    self.statistics['change_count'] += 1
                 for log in tdd_video_logs:
                     self.logger.info(f'Update video info. bvid: {bvid}, attr: {log.attr}, {log.oldval} -> {log.newval}')
-                    self.statistics['change_log_count'] += 1
-                self.logger.debug(f'{tdd_video_logs} log(s) found. bvid: {bvid}')
+                self.logger.debug(f'{len(tdd_video_logs)} log(s) found. bvid: {bvid}')
+                self.stat.condition[f'{len(tdd_video_logs)}_update'] += 1
+
             end_ts_ms = get_ts_ms()
-            cost_ms = end_ts_ms - start_ts_ms
-            self.logger.debug(f'Finish update video info. bvid: {bvid}, cost: {format_ts_ms(cost_ms)}')
-            self.statistics['total_count'] += 1
-            self.statistics['total_cost_ms'] += cost_ms
+            duration_ms = end_ts_ms - start_ts_ms
+            self.logger.debug(f'Finish update video info. bvid: {bvid}, cost: {format_ts_ms(duration_ms)}')
+            self.stat.total_count += 1
+            self.stat.total_duration_ms += duration_ms
+
+    def cleanup(self):
         self.session.close()
-        self.logger.info(f'Job end.')
