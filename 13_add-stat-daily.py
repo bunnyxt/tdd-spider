6b3421fdf4502710a42e31470fc254eafcdeb04c
bunnyxt <bunnyxt@outlook.com>
2023-07-08 00:20:58 -0700

    refactor: keep consistency with latest coding style

--- 13_add-stat-daily.py
+++ 13_add-stat-daily.py
@@ -2,6 +2,7 @@ import logging
 from logutils import logging_init
 from util import get_ts_s
 from db import Session, DBOperation, TddStatDaily
+
 logger = logging.getLogger('13')
 
 
@@ -15,16 +16,17 @@ def add_stat_daily():
         member_count = DBOperation.count_table_until_ts('tdd_member', added, session)
         video_record_count = DBOperation.count_table_until_ts('tdd_video_record', added, session)
 
-        new_stat_daily = TddStatDaily()
-        new_stat_daily.added = added
-        new_stat_daily.video_count = video_count
-        new_stat_daily.member_count = member_count
-        new_stat_daily.video_record_count = video_record_count
+        new_stat_daily = TddStatDaily(
+            added=added,
+            video_count=video_count,
+            member_count=member_count,
+            video_record_count=video_record_count
+        )
         DBOperation.add(new_stat_daily, session)
 
-        logger.info('Finish add stat daily %r!' % new_stat_daily)
+        logger.info(f'Finish add stat daily! {new_stat_daily}')
     except Exception as e:
-        logger.warning('Fail to add stat daily! Exception caught. Detail: %s' % e)
+        logger.error(f'Fail to add stat daily! error: {e}')
 
     session.close()
 
