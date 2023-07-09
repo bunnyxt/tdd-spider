056c5a9f2f83c6f2d2c6334bd6e2b6206e03af55
bunnyxt <bunnyxt@outlook.com>
2023-07-08 18:25:58 -0700

    feat: remove legacy unused spider

--- spider/custom/__init__.py
+++
@@ -1,4 +0,0 @@
-from .fetcher import ApiFetcher
-from .parser import TddMemberFollowerRecordParser, TddVideoRecordParser
-from .saver import FileSaver, DbSaver, DbListSaver
-from .proxieser import LocalProxieser
