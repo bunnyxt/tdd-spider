af53a5432e328cdfbfbcd23524a3906bf1e12cf4
bunnyxt <bunnyxt@outlook.com>
2023-07-09 00:23:32 -0700

    feat: add Timer

---
+++ timer/Timer.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from util import get_ts_ms, ts_ms_to_str, format_ts_ms
+
+__all__ = ['Timer']
+
+
+class Timer:
+    def __init__(self):
+        self.start_ts_ms: Optional[int] = None
+        self.end_ts_ms: Optional[int] = None
+
+    def start(self):
+        self.start_ts_ms = get_ts_ms()
+
+    def end(self):
+        self.end_ts_ms = get_ts_ms()
+
+    def get_duration_ms(self) -> Optional[int]:
+        if self.start_ts_ms is None or self.end_ts_ms is None:
+            return None
+        return self.end_ts_ms - self.start_ts_ms
+
+    def get_summary(self) -> str:
+        duration_ms = self.get_duration_ms()
+        if duration_ms is None:
+            return 'Timer not started or ended.'
+        return f'start: {ts_ms_to_str(self.start_ts_ms)}, ' \
+               f'end: {ts_ms_to_str(self.end_ts_ms)}, ' \
+               f'duration: {format_ts_ms(duration_ms)}'
