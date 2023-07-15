e71a3d5e4fa6a82aea6fec84dbc3df6a3ca08c08
bunnyxt <bunnyxt@outlook.com>
2023-07-15 12:07:36 -0700

    feat: add sc_send_summary

--- serverchan/serverchan.py
+++ serverchan/serverchan.py
@@ -1,11 +1,14 @@
 import urllib3
 import json
 from conf import get_sckey
+from timer import Timer
+from job import JobStat
+from util import get_ts_s_str
 import logging
 
 logger = logging.getLogger('serverchan')
 
-__all__ = ['sc_send', 'sc_send_critical']
+__all__ = ['sc_send', 'sc_send_summary', 'sc_send_critical']
 
 http = urllib3.PoolManager()
 
@@ -35,6 +38,15 @@ def sc_send(text, desp=None):
         logger.warning('Exception occurred when send message %s. Detail: %s' % (message, e))
     finally:
         return sc_response
+
+
+def sc_send_summary(script_id: str, script_name: str, timer: Timer, stat: JobStat):
+    title = f'SUMMARY: {script_id} - {script_name})'
+    desc = f'# {script_id} - {script_name} done!\n\n' \
+           f'{timer.get_summary()}\n\n' \
+           f'{stat.get_summary()}\n\n' \
+           f'by bunnyxt, {get_ts_s_str()}'
+    sc_send(title, desc)
 
 
 def sc_send_critical(title, message, file_name, line_no):
