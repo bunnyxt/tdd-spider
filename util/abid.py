6ebb102573fb36ed437c19cbd986224856529f82
bunnyxt <bunnyxt@outlook.com>
2024-12-26 17:38:17 -0500

    fix: update a2b b2a algorithm

--- util/abid.py
+++ util/abid.py
@@ -1,35 +1,52 @@
 __all__ = ['b2a', 'a2b']
 
-# ref: https://www.zhihu.com/question/381784377/answer/1099438784
-table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
-tr = {}
-for i in range(58):
-    tr[table[i]] = i
-s = [11, 10, 3, 8, 4, 6]
-xor = 177451812
-add = 8728348608
+# ref: https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/bvid_desc.md#python
+
+XOR_CODE = 23442827791579
+MASK_CODE = 2251799813685247
+MAX_AID = 1 << 51
+ALPHABET = "FcwAPNKTMug3GV5Lj7EJnHpWsx4tb8haYeviqBz6rkCy12mUSDQX9RdoZf"
+ENCODE_MAP = 8, 7, 0, 5, 1, 3, 2, 4, 6
+DECODE_MAP = tuple(reversed(ENCODE_MAP))
+
+BASE = len(ALPHABET)
+PREFIX = "BV1"
+PREFIX_LEN = len(PREFIX)
+CODE_LEN = len(ENCODE_MAP)
 
 
-def b2a(x: str) -> int:
+def b2a(bvid: str) -> int:
     """
     Convert bvid to aid.
-    :param x: bvid string without BV prefix
+    :param bvid: bvid string without BV prefix
     :return: aid
     """
-    r = 0
-    for i in range(6):
-        r += tr[x[s[i] - 2]] * 58 ** i  # no BV prefix provided
-    return (r - add) ^ xor
+    bvid = bvid[1:]
+    tmp = 0
+    for i in range(CODE_LEN):
+        idx = ALPHABET.index(bvid[DECODE_MAP[i]])
+        tmp = tmp * BASE + idx
+    return (tmp & MASK_CODE) ^ XOR_CODE
 
 
-def a2b(x: int) -> str:
+def a2b(aid: int) -> str:
     """
     Convert aid to bvid.
-    :param x: aid
+    :param aid: aid
     :return: bvid string without BV prefix
     """
-    x = (x ^ xor) + add
-    r = list('BV1  4 1 7  ')
-    for i in range(6):
-        r[s[i]] = table[x // 58 ** i % 58]
-    return ''.join(r)[2:]  # remove BV prefix
+    bvid = [""] * 9
+    tmp = (MAX_AID | aid) ^ XOR_CODE
+    for i in range(CODE_LEN):
+        bvid[ENCODE_MAP[i]] = ALPHABET[tmp % BASE]
+        tmp //= BASE
+    return (PREFIX + "".join(bvid))[2:]  # remove BV prefix
+
+
+# int aid
+assert a2b(456930) == '19x411F7kL'
+assert b2a('19x411F7kL') == 456930
+
+# bigint aid
+assert a2b(113672802272752) == '1HWkwYWETx'
+assert b2a('1HWkwYWETx') == 113672802272752
